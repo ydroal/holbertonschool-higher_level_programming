4158371ed1321a885ba9136909519bb76e3fd45e
ydroal <droal.yoko@gmail.com>
2023-01-17 08:19:49 -0800

    add: task8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        uc = ord(i)
+        if ord(i) >= 97 and ord(i) <= 122:
+            uc = uc - 32
+        print('{:c}'.format(uc), end='')
