09fc465d47d273d89d58be59759d95f2b8e24c36
ydroal <droal.yoko@gmail.com>
2023-01-19 05:59:55 -0800

    fix: task13

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     for i in range(122, 96, -1):
         if i % 2 != 0:
             i = i - 32
-        print(chr(i), end='')
+        print('{}'.format(chr(i)), end='')
