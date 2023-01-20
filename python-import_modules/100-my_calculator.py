4e8ae9a1bc3e2f8d11248c038c1da0317dd9510d
ydroal <droal.yoko@gmail.com>
2023-01-20 05:32:41 -0800

    fix: task6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
-    len = len(sys.argv)
+    len = len(argv)
     op = ['+', '-', '*', '/']
 
     if len != 4:
-        sys.stderr.write('Usage: ./100-my_calculator.py <a> <operator> <b>\n')
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
 
-    if not sys.argv[2] in op:
-        sys.stderr.write(
-            'Unknown operator. Available operators: +, -, * and /\n')
+    if not argv[2] in op:
+        print(
+            'Unknown operator. Available operators: +, -, * and /')
         exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
     res = 0
-    if sys.argv[2] == op[0]:
+    if argv[2] == op[0]:
         res = a + b
-    elif sys.argv[2] == op[1]:
+    elif argv[2] == op[1]:
         res = a - b
-    elif sys.argv[2] == op[2]:
+    elif argv[2] == op[2]:
         res = a * b
-    elif sys.argv[2] == op[3]:
+    elif argv[2] == op[3]:
         res = a / b
 
-    print('{} {} {} = {}'.format(a, sys.argv[2], b, res))
+    print('{} {} {} = {}'.format(a, argv[2], b, res))
     exit
