0beab0befe4a646eb6a725f4c17e13204d4df126
ydroal <droal.yoko@gmail.com>
2023-01-24 01:24:19 -0800

    fix: task10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,5 +2,6 @@
 
 
 def divisible_by_2(my_list=[]):
-    new_list = [True if my_list[i] % 2 == 0 else False for i in my_list]
+    new_list = [True if my_list[i] %
+                2 == 0 else False for i in range(len(my_list))]
     return new_list
