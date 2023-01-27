f5e10c9101e2f278cb02e08a26a1c75cf35448a9
ydroal <droal.yoko@gmail.com>
2023-01-27 02:18:57 -0800

    modify: task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     res = 0
-    for i in range(x):
-        try:
+    try:
+        for i in range(x):
             print('{}'.format(my_list[i]), end='')
             res += 1
-        except IndexError:
-            break
+    except IndexError:
+        pass
     print()
     return res
