c1fd87dc187d815310e10ea2adbc297012d88b13
ydroal <droal.yoko@gmail.com>
2023-01-27 12:47:17 -0800

    fix: task7

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -6,8 +6,12 @@ def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
         return (True)
-    except (ValueError, TypeError):
+    except ValueError:
         sys.stderr.write(
             'Exception: Unknown format code \'d\''
             ' for object of type \'str\'\n')
+        return (False)
+    except TypeError:
+        sys.stderr.write(
+            'Exception: unsupported format string passed to set.__format__\n')
         return (False)
