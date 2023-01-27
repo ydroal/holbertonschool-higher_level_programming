834600477a2ff500d22761e7236e53da3322b80b
ydroal <droal.yoko@gmail.com>
2023-01-27 11:42:49 -0800

    add: task7

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+        return (True)
+    except ValueError:
+        sys.stderr.write(
+            'Exception: Unknown format code \'d\' 
+	    for object of type \'str\'\n')
+        return (False)
