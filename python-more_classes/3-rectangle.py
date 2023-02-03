b39765a3ce64fa7d51bd2e176261ab6e7e59fd28
ydroal <droal.yoko@gmail.com>
2023-02-03 03:00:28 -0800

    add: task3

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+'''Module to define a rectangle'''
+
+
+class Rectangle:
+    '''Define a rectangle'''
+
+    def __init__(self, width=0, height=0):
+        '''Initialize rectangle with a given size.'''
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''
+        Getter method for the value of `__width`.
+        Returns:
+            int: The value of __width.
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''Setter method for the 'width' property.'''
+
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        '''
+        Getter method for the value of `__height`.
+        Returns:
+            int: The value of __height.
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Setter method for the 'height' property.'''
+
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    def area(self):
+        '''Method that returns the rectangle area'''
+
+        return self.__height * self.__width
+
+    def perimeter(self):
+        '''Method that returns the rectangle perimeter'''
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        '''Method that return a string to print a rectangle.'''
+
+        if self.__width == 0 or self.__height == 0:
+            return ''
+
+        else:
+            return '\n'.join(('#' * self.__width)
+                             for _ in range(self.__height))
