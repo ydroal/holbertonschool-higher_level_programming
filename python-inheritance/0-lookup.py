057c619ac7ad8298e1fc9c964e346333681b6490
ydroal <droal.yoko@gmail.com>
2023-02-06 07:52:05 -0800

    add: task0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module to define a lookup function'''
+
+
+def lookup(obj):
+    '''
+    Function that returns the list of available attributes 
+    and methods of an object.
+    '''
+    return dir(obj)
