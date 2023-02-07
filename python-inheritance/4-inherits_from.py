e6863d2cad43541089200d19c34e36abecf2848c
ydroal <droal.yoko@gmail.com>
2023-02-07 06:00:50 -0800

    add: task4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module to define a inherits_from function'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    Function that True if the object is an instance of a class
+     that inherited (directly or indirectly) from the specified class
+     ; otherwise False.
+    '''
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
