d79b81fce796d5976e328d1e65207df85ab5cf3e
ydroal <droal.yoko@gmail.com>
2023-02-07 01:22:47 -0800

    fix: task3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module to define a is_kind_of_clas function'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    Function that returns True if the object is an instance of,
+     or if the object is an instance of a class that inherited from,
+     the specified class ; otherwise False.
+    '''
+    return isinstance(obj, a_class)
