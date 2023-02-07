9e02433d4d24c2be154c666864edf9352f95aadf
ydroal <droal.yoko@gmail.com>
2023-02-07 06:26:45 -0800

    add: task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module to define a class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''Define a BaseGeometry'''
+
+    def area(self):
+        '''Method that raises an Exception with the message'''
+
+        raise Exception('area() is not implemented')
