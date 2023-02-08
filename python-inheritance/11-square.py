db646e143c17a704bb7b7b7ce0697cc8b059f887
ydroal <droal.yoko@gmail.com>
2023-02-08 01:36:06 -0800

    add: task11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Module to define a class Square'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Define a Square'''
+
+    def __init__(self, size):
+        '''Initialize Square'''
+
+        super(Rectangle, self).integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        '''Overriding method that return area of square'''
+
+        return self.__size**2
+
+    def __str__(self):
+        '''Overriding method that return a string to print'''
+
+        return ('[Square] {}/{}'.format(self.__size, self.__size))
