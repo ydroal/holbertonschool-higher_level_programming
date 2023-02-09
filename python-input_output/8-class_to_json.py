21f6d4d86123df93e818f011dd720e76031754a5
ydroal <droal.yoko@gmail.com>
2023-02-09 03:29:40 -0800

    add: task8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Module to define a function class_to_json'''
+
+
+def class_to_json(obj):
+    '''
+    Function that returns the dictionary description with
+    simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    '''
+
+    return obj.__dict__
