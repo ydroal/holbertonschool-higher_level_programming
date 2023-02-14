463cbf9ce399961326106319e6728962879c2626
ydroal <droal.yoko@gmail.com>
2023-02-14 06:46:26 -0800

    add: task11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -15,3 +15,22 @@ class Square(Rectangle):
 
         return (f'[Square] ({self.id}) {self.x}/{self.y} - '
                 f'{self.width}')
+
+    @property
+    def size(self):
+        '''Returns size'''
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''Setter method for size'''
+
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+
+        if value <= 0:
+            raise ValueError('width must be > 0')
+
+        self.width = value
+        self.height = value
