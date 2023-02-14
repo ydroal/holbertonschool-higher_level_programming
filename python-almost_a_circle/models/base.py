73c922dabe4e3b7517b463f02b55fb48a6298dab
ydroal <droal.yoko@gmail.com>
2023-02-14 13:51:51 -0800

    add: task15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''Module to define a class Base'''
 
+import json
+
 
 class Base:
     '''Define a Base'''
@@ -16,3 +18,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Method that returns the JSON string representation of list'''
+
+        return json.dumps(list_dictionaries)
