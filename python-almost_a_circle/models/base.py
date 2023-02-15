c4478d9f1bf06d69873dc20ee54187eb7b26c01d
ydroal <droal.yoko@gmail.com>
2023-02-15 01:46:33 -0800

    add: task16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,6 +19,30 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        Method that writes the JSON string representation
+        of list_objs to a file
+        '''
+
+        if list_objs is None:
+            list_objs = []
+
+        class_name = cls.__name__
+        file_name = class_name + '.json'
+
+        # Convert list_objs to list of dictionaries
+        dict_list = []
+        for obj in list_objs:
+            dict_list.append(obj.to_dictionary())
+
+        # Convert list of dictionaries to JSON string
+        json_string = cls.to_json_string(dict_list)
+
+        with open(file_name, 'w') as json_file:
+            json_file.write(json_string)
+
     @staticmethod
     def to_json_string(list_dictionaries):
         '''Method that returns the JSON string representation of list'''
