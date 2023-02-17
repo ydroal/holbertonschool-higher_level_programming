7ff2dbd289da661a5a638b6cb4b6e1e2cd3e46ae
ydroal <droal.yoko@gmail.com>
2023-02-17 02:11:55 -0800

    update: test_square

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -27,12 +27,16 @@ class Test_Square(unittest.TestCase):
         self.assertEqual(s1.x, 2)
         self.assertEqual(s1.y, 0)
 
+        with self.assertRaises(TypeError) as e:
+            s2 = Square('1')
+        self.assertEqual(str(e.exception), 'width must be an integer')
+
         with self.assertRaises(ValueError) as e:
-            s2 = Square(0)
+            s3 = Square(0)
         self.assertEqual(str(e.exception), 'width must be > 0')
 
         with self.assertRaises(ValueError) as e:
-            s3 = Square(-1)
+            s4 = Square(-1)
         self.assertEqual(str(e.exception), 'width must be > 0')
 
     def test_x(self):
@@ -74,6 +78,65 @@ class Test_Square(unittest.TestCase):
 
         s.update(size=7, id=89, y=1)
         self.assertEqual(str(s), '[Square] (89) 1/1 - 7')
+
+    def test_to_dictionary(self):
+        s = Square(10, 2, 1, 9)
+        s_dictionary = s.to_dictionary()
+        self.assertEqual(
+            s_dictionary, {'id': 9, 'size': 10, 'x': 2, 'y': 1})
+
+    def test_create(self):
+        s_dict = {'id': 89}
+        s = Square.create(**s_dict)
+        self.assertEqual(str(s), '[Square] (89) 0/0 - 10')
+
+        s_dict1 = {'id': 89, 'size': 1}
+        s1 = Square.create(**s_dict1)
+        self.assertEqual(str(s1), '[Square] (89) 0/0 - 1')
+
+        s_dict2 = {'id': 89, 'size': 1, 'x': 2}
+        s2 = Square.create(**s_dict2)
+        self.assertEqual(str(s2), '[Square] (89) 2/0 - 1')
+
+        s_dict3 = {'id': 89, 'size': 1, 'x': 2, 'y': 3}
+        s3 = Square.create(**s_dict3)
+        self.assertEqual(str(s3), '[Square] (89) 2/3 - 1')
+
+    def test_save_to_file(self):
+        s = Square(10)
+        Square.save_to_file([s])
+        with open('Square.json') as file:
+            op = file.read()
+            self.assertEqual(type(op), str)
+
+        Square.save_to_file([])
+        with open('Square.json') as file:
+            op = file.read()
+            self.assertEqual(op, '[]')
+
+        Square.save_to_file(None)
+        with open('Square.json') as file:
+            op = file.read()
+            self.assertEqual(op, '[]')
+
+    def test_load_from_file(self):
+        s = Square.load_from_file()
+        self.assertEqual(s, [])
+
+        s1 = Square(10, 7, 2, 8)
+        s2 = Square(2, 4)
+        li_Squares = [s1, s2]
+        Square.save_to_file(li_Squares)
+        out = Square.load_from_file()
+        self.assertEqual(len(out), 2)
+        self.assertEqual(type(out[0]), Square)
+        self.assertEqual(type(out[1]), Square)
+        self.assertEqual(s1.x, out[0].x)
+        self.assertEqual(s1.y, out[0].y)
+        self.assertEqual(s1.size, out[0].size)
+        self.assertEqual(s2.x, out[1].x)
+        self.assertEqual(s2.y, out[1].y)
+        self.assertEqual(s2.size, out[1].size)
 
 
 if __name__ == '__main__':
