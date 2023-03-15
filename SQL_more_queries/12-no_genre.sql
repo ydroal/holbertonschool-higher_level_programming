79ea1c0495cc0f36746e3204495c81e24dbd149a
ydroal <droal.yoko@gmail.com>
2023-03-15 03:48:39 -0700

    fix: task12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,4 @@
+-- script that lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT `tv_shows`.`title`, COALESCE(`tv_show_genres`.`genre_id`, 'NULL') AS `genre_id`
 FROM `tv_shows`
 LEFT JOIN `tv_show_genres` ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
