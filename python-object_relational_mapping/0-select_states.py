24fbc3c2b02b2972a9fb754100f25e28e6571836
ydroal <droal.yoko@gmail.com>
2023-03-16 03:51:45 -0700

    fix: task0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,24 @@
 
 
 import MySQLdb
+import sys
 
 
-# connect to database
-db = MySQLdb.connect(host='localhost', port=3306, user='root',
-                     passwd='root', db='hbtn_0e_0_usa')
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-# create cursor
-cursor = db.cursor()
+    # connect to database
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=db_name)
 
-# query
-cursor.execute('SELECT * FROM states ORDER BY id')
+    # create cursor
+    cursor = db.cursor()
 
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    # query
+    cursor.execute('SELECT * FROM states ORDER BY id')
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
