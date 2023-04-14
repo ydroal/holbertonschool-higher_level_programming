3cc5702f49b9d15caef47a91a3614c21e15364fb
ydroal <droal.yoko@gmail.com>
2023-04-14 16:14:16 -0700

    add: task7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const num = parseInt(process.argv[2]);
 if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i <= num; i++) {
+  for (let i = 0; i < num; i++) {
       console.log('C is fun')
   }
 }
