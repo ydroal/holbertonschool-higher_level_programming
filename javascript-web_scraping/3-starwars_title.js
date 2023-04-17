3ee124948e3ac6a2a2c79dbba6d57189536302ce
ydroal <droal.yoko@gmail.com>
2023-04-17 03:57:03 -0700

    add: task3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
