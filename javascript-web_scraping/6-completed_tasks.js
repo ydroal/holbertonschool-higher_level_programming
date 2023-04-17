3adeae28a27ed74b45724345c9407b5fe6e2467a
ydroal <droal.yoko@gmail.com>
2023-04-17 08:11:33 -0700

    add: task6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const res = {};
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const obj = JSON.parse(body);
+  for (let i = 1; i <= 10; i++) {
+    const filteredList = obj.filter(e => e.userId === parseInt(i)).filter(id => id.completed === true);
+    res[parseInt(i)] = filteredList.length;
+  }
+  console.log(res);
+});
